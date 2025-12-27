ac8d440cd40e864e0fe79eb4a3dadb2ecdb0eaca
ggoswami777 <gaurav.goswami1304@gmail.com>
2025-12-27 23:46:40 +0530

    added latest collection skeleton

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
-createRoot(document.getElementById('root')).render(
- 
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import ShopContextProvider from "./context/ShopContext.jsx";
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,    
-  
-)
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </BrowserRouter>
+);
