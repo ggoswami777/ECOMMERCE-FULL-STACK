d513620cf95971ef7fe40dcc93a5bfbf3c89cbfd
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-01-02 11:58:20 +0530

    added the sort functionality in collection page

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -11,6 +11,7 @@ const Collection = () => {
   const [filterProducts,setFilterProducts]=useState([]);
   const [category,setCategory]=useState([]);
   const [subCategory,setSubCategory]=useState([]);
+  const [sortType,setSortTye]=useState('relevant');
   const toggleCategory=(e)=>{
     if(category.includes(e.target.value)){
       setCategory(prev=>prev.filter(item=>item!==e.target.value))
@@ -40,9 +41,31 @@ const Collection = () => {
     setFilterProducts(productsCopy)
 
   }
+
+  const sortProduct=()=>{
+    let  fpCopy=filterProducts.slice();
+    switch(sortType){
+      case 'low-high':
+        setFilterProducts(fpCopy.sort((a,b)=>(
+a.price - b.price
+        )))
+        break;
+        case 'high-low':
+          setFilterProducts(fpCopy.sort((a,b)=>(
+b.price - a.price
+        )))
+        break;
+        default:
+          applyFilter();
+          break;
+    }
+  }
   useEffect(()=>{
     applyFilter() 
   },[category,subCategory,products])
+  useEffect(()=>{
+    sortProduct();
+  },[sortType])
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
       {/* FILTER OPTIONS */}
@@ -90,7 +113,7 @@ const Collection = () => {
         <div className='flex justify-between text-bse sm:text-2xl mb-4'>
         <Title text1={'ALL'} text2={'COLLECTIONS'}/>
         {/* product sort */}
-        <select className='border-2 border-gray-300 text-sm px-2'>
+        <select onChange={(e)=>setSortTye(e.target.value)} className='border-2 border-gray-300 text-sm px-2'>
           <option value="relavent">Sort by: Relavent</option>
           <option value="low-high">Sort by: Low to High</option>
           <option value="high-low">Sort by: High to Low</option>
