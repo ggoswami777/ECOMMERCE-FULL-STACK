49f1da4da9c92819678ff023b51272cae3242379
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-01-03 20:06:04 +0530

    added toastify and cart button showing dynamic numbers

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext= createContext();
 const ShopContextProvider=(props)=>{
@@ -7,8 +8,41 @@ const ShopContextProvider=(props)=>{
     const delivery_fee=10; 
     const [search,setSearch]=useState('');
     const [showSearch,setShowSearch]=useState(false);
+    const [cartItems,setCartItems]=useState({});
+
+    const addToCart=async (itemId,size)=>{
+        if(!size){
+            toast.error('Select product size');
+            return
+        }
+        let cartData=structuredClone(cartItems);
+        if(cartData[itemId]){
+            if(cartData[itemId][size]){
+                cartData[itemId][size]+=1;
+            }else{
+                cartData[itemId][size]=1;
+            }
+        }else{
+            cartData[itemId]={};
+            cartData[itemId][size]=1;
+
+        }
+        setCartItems(cartData);
+    }
+    const getCartCount = () => {
+    let totalCount = 0;
+
+    for (const productId in cartItems) {
+        for (const size in cartItems[productId]) {
+            totalCount += cartItems[productId][size];
+        }
+    }
+
+    return totalCount;
+};
+
     const value={
-        products,currency,delivery_fee,search,setSearch,showSearch,setShowSearch
+        products,currency,delivery_fee,search,setSearch,showSearch,setShowSearch,cartItems,addToCart,getCartCount
     }
     return(
         <ShopContext.Provider value={value}>
