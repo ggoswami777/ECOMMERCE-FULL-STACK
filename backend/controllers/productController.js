069701ec0b2d99a2a07617ab336e3aef2fc2292b
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-01-11 13:01:13 +0530

    added productController skeleton

---
+++ backend/controllers/productController.js
@@ -0,0 +1,21 @@
+
+
+
+// function for add product
+const addProduct=async(req,res)=>{
+
+}
+// function for list product
+const listProducts=async(req,res)=>{
+
+}
+// fuction for removing product
+const removeProduct=async(req,res)=>{
+
+}
+// fuction for single product info
+const singleProduct=async (req,res)=>{
+
+}
+
+export {listProducts,addProduct,singleProduct,removeProduct}
--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ app.use(cors());
 
 // api endpoints
 app.use('/api/user',userRouter);
+app.use('/api/product',productRouter);
 app.get('/',(req,res)=>{
     res.send("API WORKING")
 })
