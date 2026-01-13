f991ba82b15cf0c5bff564cbc462ebe8d6a8605c
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-01-13 16:49:45 +0530

    added admin folder/login /navbar and sidebar

---
+++ admin/src/components/Login.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='min-h-screen bg-[#ffebf5] flex items-center justify-center w-full '>
+      <div className='bg-white shadow-md rouded-lg px-8 max-w-md py-3'>
+        <h1 className='text-2xl font-bold mb=4'>Admin Panel</h1>
+        <form action="">
+            <div className='mb-3 min-w-72'>
+                <p className='text-sm font-medium text-gray-700 mb-2'>Email Address</p>
+                <input className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="text" placeholder='your@email.com' />
+            </div>
+            <div className='mb-3 min-w-72'>
+                <p className='text-sm font-medium text-gray-700 mb-2'>Password</p>
+                <input className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="text" placeholder='Enter your password' />
+            </div>
+            <button className='mt-2 w-full py-2 px-4 rounded-md text-white bg-black' type='submit'>Login</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
