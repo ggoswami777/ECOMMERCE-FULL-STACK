cc70a7edef4022c97eff141e8723632368839d89
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-01-18 21:12:52 +0530

    added the orders placed  and track feature with admin control

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -13,7 +13,7 @@ const Sidebar = () => {
                 <img className='w-5 h-5' src={assets.order_icon} alt="" />
                 <p className='hidden md:block'>List Items</p>
             </NavLink>
-            <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-lg' to="/order">
+            <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-lg' to="/orders">
                 <img className='w-5 h-5' src={assets.order_icon} alt="" />
                 <p className='hidden md:block'>Orders</p>
             </NavLink>
